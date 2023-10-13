00f7103779b8d0d6a2d07fa520ba56ad6163786e
Ömer Geysoğlu <ogeysoglu@outlook.com>
2023-10-13 11:49:44 +0300

    question added

--- main.py
+++ main.py
@@ -7,24 +7,33 @@ def main():
     cap = cv2.VideoCapture(0)
     hand_landmarker = rfp.FingerCounter()
     question_generator = QuestionGenerator()
-    num1, num2, correct_answer = question_generator.generate_question()
     numRaised = 0
-
+    question = question_generator.generate_question()
+    frame_counter_next_question = 0
+    isCorrectlyAnswered = False
     while True:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-
+        cv2.blur(frame, (4,4))
         hand_landmarker.detect_async(frame)
         frame = rfp.draw_landmarks_on_image(frame,hand_landmarker.result)
         frame, numRaised = rfp.count_fingers_raised(frame,hand_landmarker.result)
-        question = "What should be in the missing number _ x {} = {}?".format(num2,correct_answer)
+        
         frame = cv2.putText(img = frame, text = question, 
                            org = (10, 50), fontFace = cv2.FONT_HERSHEY_SIMPLEX,
                            fontScale = 0.6, color = (0,0,255), thickness = 2, lineType = cv2.LINE_AA)
-        if(numRaised == num1):
+        isAnswerCorrect = question_generator.checkAnswer(numRaised)
+        if(isAnswerCorrect):
             frame = cv2.putText(img = frame, text = "Correct!", 
                            org = (10, 100), fontFace = cv2.FONT_HERSHEY_SIMPLEX,
                            fontScale = 0.6, color = (0,255,0), thickness = 2, lineType = cv2.LINE_AA)
+            isCorrectlyAnswered = True
+        if(isCorrectlyAnswered):
+            frame_counter_next_question += 1
+            if(frame_counter_next_question > 40):
+                frame_counter_next_question = 0
+                isCorrectlyAnswered = False
+                question = question_generator.generate_question()
             
             
                  
--- question_generator.py
+++ question_generator.py
@@ -4,9 +4,17 @@ class QuestionGenerator:
     def __init__(self, min_number=1, max_number=10):
         self.min_number = min_number
         self.max_number = max_number
+        self.num1 = 0
+        self.num2 = 0
 
     def generate_question(self):
-        num1 = random.randint(self.min_number, self.max_number)
-        num2 = random.randint(self.min_number, self.max_number)
-        correct_answer = num1 * num2
-        return num1, num2, correct_answer
+        self.num1 = random.randint(self.min_number, self.max_number)
+        self.num2 = random.randint(self.min_number, self.max_number)
+        correct_answer = self.num1 * self.num2
+        return "What should be in the missing number _ x {} = {}?".format(self.num2,correct_answer)
+    
+    def checkAnswer(self,numRaised):
+        if(numRaised == self.num1):
+            return True
+        else:
+            return False
